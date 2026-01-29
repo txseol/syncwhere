3fd84b6825f47fb5183c063de3f27ef9c42f0ec8
한규현 <tx.seol@gmail.com>
2026-01-30 02:41:40 +0900

    schema unique add dir

---
+++ prisma/migrations/20260129174112/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[channel_id,parent_id,name,dir]` on the table `document_data` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX "document_data_channel_id_parent_id_name_key";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "document_data_channel_id_parent_id_name_dir_key" ON "document_data"("channel_id", "parent_id", "name", "dir");
